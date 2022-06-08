62919c1d74a14d09fda1d3bb61344ba0f7d0b28f
diegoguilhermepassos <diegoguilhermepassos@github.com>
2022-06-08 12:17:44 -0300

    migrations + model interface

--- help-x-back/controllers/book_controller.go
+++ help-x-back/controllers/book_controller.go
@@ -26,11 +26,56 @@ func ShowBook(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(400, gin.H{
-			"error": "cannot fin book: " + err.Error(),
+			"error": "cannot find book: " + err.Error(),
 		})
 
 		return
 
 	}
 	c.JSON(200, book)
+}
+
+func CreateBook(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var book models.Book
+
+	err := c.ShouldBindJSON(&book)
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot bind JSON: " + err.Error(),
+		})
+
+		return
+	}
+
+	err = db.Create(&book).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot create book: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
+}
+
+func ShowBooks(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var books []models.Book
+	err := db.Find(&books).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot list books: " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, books)
 }
--- help-x-back/database/database.go
+++ help-x-back/database/database.go
@@ -11,7 +11,7 @@ import (
 var db *gorm.DB
 
 func StartDB() {
-	str := "host=localhost port=25432 user=admin dbname=books sslmode=disable password=123456s"
+	str := "host=localhost port=3306 user=root dbname=db_bolt sslmode=disable password=123456"
 
 	database, err := gorm.Open(postgres.Open(str), &gorm.Config{})
 	if err != nil {
